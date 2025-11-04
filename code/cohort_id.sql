8290317147330ef88cc08911c2e21229844f9eb7
Zewei (Whiskey) Liao <whiskey0504@gmail.com>
2025-11-03 21:08:03 -0600

    feat: finish constructing the dataset

--- code/cohort_id.sql
+++ code/cohort_id.sql
@@ -10,12 +10,17 @@ WITH t1 AS (
             -- getting on imv (intub)
             OR (_on_imv = 1 AND _on_imv IS DISTINCT FROM LAG(_on_imv) OVER w)
             THEN 1 ELSE 0 END
+        , _trach_flip_to_1: CASE
+            WHEN tracheostomy = 1 AND tracheostomy IS DISTINCT FROM LAG(tracheostomy) OVER w THEN 1 ELSE 0 END
     WINDOW w AS (PARTITION BY hospitalization_id ORDER BY event_dttm)    
 )
 , t2 as (
     FROM t1
     SELECT *
         , _streak_id: SUM(_chg_imv) OVER w
+        , _trach_flip_cumsum: SUM(_trach_flip_to_1) OVER w
+        , _trach_1st: CASE
+            WHEN _trach_flip_to_1 = 1 AND _trach_flip_cumsum = 1 THEN 1 ELSE 0 END
     WINDOW w AS (PARTITION BY hospitalization_id ORDER BY event_dttm)
 )
 , all_streaks as (
@@ -24,6 +29,7 @@ WITH t1 AS (
         , _streak_id
         , _start_dttm: MIN(event_dttm)
         , _last_observed_dttm: MAX(event_dttm)
+        , _trach_dttm: MIN(CASE WHEN _trach_1st = 1 THEN event_dttm END)
         , _on_imv: MAX(_on_imv)
     -- WHERE _on_imv = 1
     GROUP BY hospitalization_id, _streak_id
@@ -33,7 +39,7 @@ WITH t1 AS (
     SELECT *
         -- the end time of the current streak is the start time of the next streak
         , _next_start_dttm: LEAD(_start_dttm) OVER w
-        , _end_dttm: COALESCE(_next_start_dttm, _last_observed_dttm)
+        , _end_dttm: COALESCE(_trach_dttm, _next_start_dttm, _last_observed_dttm)
         , _duration_hrs: date_diff('minute', _start_dttm, _end_dttm) / 60
         , _at_least_24h: CASE WHEN _duration_hrs >= 24 THEN 1 ELSE 0 END
     WINDOW w AS (PARTITION BY hospitalization_id ORDER BY _streak_id)
